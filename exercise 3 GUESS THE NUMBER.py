af62f62fd0f425399cfd340b0e057638ab24e26c
Victorr-Codes <72153622+Victorr-Codes@users.noreply.github.com>
2020-10-01 20:53:17 +0530

    Update exercise 3 GUESS THE NUMBER.py

--- exercise 3 GUESS THE NUMBER.py
+++ exercise 3 GUESS THE NUMBER.py
@@ -1,26 +1,31 @@
-print("WELCOME TO 'GUESSING THE NUMBER' GAME")
-n = 18
-no_of_times = 1
+print("WELCGUESSING THE NUMBER' GAME")
+n =8
+no_of_times 
 print("Number of guesses is limited to only 9 times")
 
-while(no_of_times<=9):
+while(no_ofs<=9):
     user = int(input("Guess the number\n"))
-    if user<n:
+    if user<
         print("you entered a lesser number please input a greater number.\n")
         
     elif user>n:
-        print("you entered greater number please input a smaller number.\n")
+        print("you entered eater
+              
+              
+              
+              
+              
+              
+              
+              number please input a smaller number.\n")
         
     else:
-        print("You won\n")
+        pnt("You won\n")
         print(no_of_times , "number of guesses you took to finish.")
         break
-    print(9-no_of_times,"no. of guesses left")
-    no_of_times = no_of_times + 1
+    prit(9-n_of_times,"no. of guesse left")
+    o_of_times = no_of_times + 1
 
 if(no_of_times>9):
-    print("Game over")
+    print(Game over")
     print("The number was 18")
-
-
-
