031312ae7b572d244647e5c69830ab86d806c0af
Alison-Code <72189697+Alison-Code@users.noreply.github.com>
2020-10-01 21:05:06 +0530

    Update exercise 3 GUESS THE NUMBER.py

--- exercise 3 GUESS THE NUMBER.py
+++ exercise 3 GUESS THE NUMBER.py
@@ -1,31 +1,26 @@
-print("WELCGUESSING THE NUMBER' GAME")
-n =8
-no_of_times 
-print("Number of guesses is limited to only 9 times")
+print("WELCOME TO 'GUESSING THE NUMBER' GAME")
+print("NOTE-IN THIS GAME YOU HAVE TO GUESS A SECRET NUMBER BY SOME HINTS")
+n = 50
+no_of_times = 1
+var1 = int(input("I how many guesses you can guess the number.\n"))
+print("You have",var1,"number of guesses")
 
-while(no_ofs<=9):
+while(no_of_times<=var1):
     user = int(input("Guess the number\n"))
-    if user<
+    if user<n:
         print("you entered a lesser number please input a greater number.\n")
+
         
     elif user>n:
-        print("you entered eater
-              
-              
-              
-              
-              
-              
-              
-              number please input a smaller number.\n")
+        print("you entered greater number please input a smaller number.\n")
         
     else:
-        pnt("You won\n")
+        print("You won\n")
         print(no_of_times , "number of guesses you took to finish.")
         break
-    prit(9-n_of_times,"no. of guesse left")
-    o_of_times = no_of_times + 1
+    print(var1-no_of_times,"number of guesses left")
+    no_of_times = no_of_times + 1
 
-if(no_of_times>9):
-    print(Game over")
+if(no_of_times>var1):
+    print("Game over")
     print("The number was 18")
